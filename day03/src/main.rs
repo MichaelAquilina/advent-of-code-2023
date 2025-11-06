73a550bb66e8a3b2d9f5aea03009ce61d2cfb919
Michael Aquilina <michaelaquilina@gmail.com>
2025-11-06 13:53:44 +0000

    day03

---
+++ day03/src/main.rs
@@ -0,0 +1,135 @@
+mod schematic;
+
+use schematic::Schematic;
+use std::error::Error;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buffer = String::new();
+
+    std::io::stdin().read_to_string(&mut buffer)?;
+    let schematic: Schematic = buffer.parse()?;
+
+    let part1 = get_part1(&schematic);
+    println!("Part 1: {}", part1);
+
+    Ok(())
+}
+
+fn get_part1(schematic: &Schematic) -> u32 {
+    let mut total = 0;
+    for (point, number) in schematic.numbers.iter() {
+        if schematic.is_part_number(point) {
+            total += number;
+        }
+    }
+    total
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use schematic::Point;
+
+    #[test]
+    fn test_corner_case() -> Result<(), Box<dyn Error>> {
+        let example = [
+            "12..+..",
+            "..34...",
+            "..@56..",
+            "78.....",
+            ".......",
+        ].join("\n");
+
+        let schematic: Schematic = example.parse()?;
+        let result = get_part1(&schematic);
+        assert_eq!(result, 168);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_no_part_symbols() -> Result<(), Box<dyn Error>> {
+        let example = ["...", ".6.", "..."].join("\n");
+        let schematic: Schematic = example.parse()?;
+        let result = get_part1(&schematic);
+        assert_eq!(result, 0);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_symbols() -> Result<(), Box<dyn Error>> {
+        let example = ["fdwdawd", "f82+18o", "fhello+"].join("\n");
+        let schematic: Schematic = example.parse()?;
+        let result = get_part1(&schematic);
+        assert_eq!(result, 100);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part1_example() -> Result<(), Box<dyn Error>> {
+        let example = [
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598..",
+        ]
+        .join("\n");
+        let schematic: Schematic = example.parse()?;
+        let result = get_part1(&schematic);
+        assert_eq!(result, 4361);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_schematic() -> Result<(), Box<dyn Error>> {
+        let example = [
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598..",
+        ]
+        .join("\n");
+        let schematic: Schematic = example.parse()?;
+        let point = Point { x: 0, y: 1 };
+        assert_eq!(schematic.get_neighbors(&point), vec![]);
+        let point = Point { x: 0, y: 0 };
+        let neighbors = schematic.get_neighbors(&point);
+        assert_eq!(schematic.symbols.len(), 6);
+        assert_eq!(schematic.numbers.len(), 10);
+        assert_eq!(
+            neighbors,
+            [
+                Point { x: -1, y: -1 },
+                Point { x: -1, y: 0 },
+                Point { x: -1, y: 1 },
+                Point { x: 0, y: -1 },
+                Point { x: 0, y: 1 },
+                Point { x: 1, y: -1 },
+                Point { x: 1, y: 1 },
+                Point { x: 2, y: -1 },
+                Point { x: 2, y: 1 },
+                Point { x: 3, y: -1 },
+                Point { x: 3, y: 0 },
+                Point { x: 3, y: 1 },
+            ]
+        );
+
+        Ok(())
+    }
+}
---
+++ day03/src/schematic.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Debug)]
+pub struct Schematic {
+    pub numbers: HashMap<Point, u32>,
+    pub symbols: HashMap<Point, char>,
+}
+
+impl Schematic {
+    pub fn is_part_number(&self, point: &Point) -> bool {
+        for neighbor in self.get_neighbors(point) {
+            if self.get_symbol(&neighbor).is_some() {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn get_neighbors(&self, point: &Point) -> Vec<Point> {
+        let mut neighbors = vec![];
+        let x = point.x;
+        let y = point.y;
+        if let Some(number) = self.numbers.get(point) {
+            let length = number.to_string().len();
+            neighbors.extend([
+                Point { x: x - 1, y: y - 1 },
+                Point { x: x - 1, y },
+                Point { x: x - 1, y: y + 1 },
+            ]);
+            for index in 0..length {
+                let x = x + (index as i32);
+                neighbors.push(Point { x, y: y - 1 });
+                neighbors.push(Point { x, y: y + 1 });
+            }
+            let x = x + (length as i32);
+            neighbors.extend([
+                Point { x, y: y - 1 },
+                Point { x, y },
+                Point { x, y: y + 1 },
+            ])
+        }
+
+        neighbors
+    }
+
+    pub fn get_symbol(&self, point: &Point) -> Option<&char> {
+        self.symbols.get(point)
+    }
+}
+
+impl std::str::FromStr for Schematic {
+    type Err = Box<dyn Error>;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let mut numbers: HashMap<Point, u32> = HashMap::new();
+        let mut symbols: HashMap<Point, char> = HashMap::new();
+
+        for (y, line) in value.lines().enumerate() {
+            let mut number_tokens: Vec<char> = vec![];
+            let mut point: Option<Point> = None;
+            for (x, token) in line.char_indices() {
+                let x = x as i32;
+                let y = y as i32;
+                if token.is_ascii_digit() {
+                    number_tokens.push(token);
+                    point.get_or_insert(Point { x, y });
+                } else {
+                    if let Some(key) = point {
+                        let number: u32 = number_tokens.iter().collect::<String>().parse()?;
+                        numbers.entry(key).or_insert(number);
+
+                        point = None;
+                        number_tokens.clear();
+                    }
+
+                    if token != '.' {
+                        let point = Point { x, y };
+                        symbols.entry(point).or_insert(token);
+                    }
+                }
+            }
+        }
+        Ok(Schematic { numbers, symbols })
+    }
+}
